2f98c49b96dcb0871d275eb596538ce8d6ec75e3
Wufe <wufe92@gmail.com>
2016-10-31 09:52:24 +0100

    Added the-control-group/voyager. Changed configuration nginx to serve storage path.

--- config/app.php
+++ config/app.php
@@ -178,6 +178,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\JsonResponseProvider::class,
+        TCG\Voyager\VoyagerServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
     ],
 
